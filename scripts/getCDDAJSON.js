6393c4a59b8af0b04e30777dc920b9e8a8ad71b8
Lin Onetwo <linonetwo012@gmail.com>
2020-12-08 00:25:20 +0800

    feat: Update script now it scan all json from CDDA

--- scripts/getCDDAJSON.js
+++ scripts/getCDDAJSON.js
@@ -2,41 +2,58 @@ const { assert } = require('console');
 const fs = require('fs-extra');
 const path = require('path');
 
-const base = '/Users/lindongwu/Desktop/github/Cataclysm-DDA/data/json/items';
-const out = '/Users/lindongwu/Desktop/github/Eternity-Foundation/scripts/out.json';
-const nameFilter = 'terrain';
-const type = 'ARMOR';
-let files = fs.readdirSync(base);
-// files = files.filter((name) => name.startsWith(nameFilter));
+const base = '/Users/linonetwo/Desktop/repo/Cataclysm-DDA/data/json';
+const mods = '/Users/linonetwo/Desktop/repo/Cataclysm-DDA/data/mods';
+const out = '/Users/linonetwo/Desktop/repo/Eternity-Foundation/scripts/out.json';
 
+const type = 'GENERIC';
+let files = [
+  ...fs.readdirSync(base).map((name) => path.join(base, name)),
+  ...fs.readdirSync(mods).map((name) => path.join(mods, name)),
+].filter((name) => !name.endsWith('replacements.json'));
+
+const isFolder = (name) => !name.endsWith('.json') && !name.endsWith('.png') && !name.endsWith('.txt') && !name.endsWith('.md')
 // deal with sub folders
-const subFolders = files.filter((name) => !name.endsWith('.json'));
-for (const subFolderName of subFolders) {
-  const subFileNames = fs.readdirSync(path.join(base, subFolderName));
-  files = [...files, ...subFileNames.map((subFileName) => path.join(subFolderName, subFileName))];
+const subFolders = files.filter(isFolder);
+while (subFolders.length > 0) {
+  const subFolderName = subFolders.pop();
+  const subFileNames = fs.readdirSync(subFolderName).map((name) => path.join(subFolderName, name));
+  const newSubFolders = subFileNames.filter(isFolder);
+  const newFiles = subFileNames.filter((name) => name.endsWith('.json'));
+  newSubFolders.forEach((newSubFolderName) => subFolders.push(newSubFolderName));
+  files = [...files, ...newFiles];
 }
 files = files.filter((name) => name.endsWith('.json'));
 
-const contents = files.flatMap((name) => fs.readJsonSync(path.join(base, name)));
-const useAction = {}
+let contents = files.flatMap((name) => fs.readJsonSync(name));
+// Collect types
+const types = new Set()
+contents.forEach((dataObject) => {
+  types.add(dataObject.type)
+})
+console.log(types)
+
+// const useAction = {}
+contents = contents.filter((item) => item.type === type);
 contents.forEach((dataObject) => {
   // if (dataObject.type !== type) {
   //   console.log(dataObject.type, dataObject);
   // }
-  if (dataObject?.use_action && !Array.isArray(dataObject.use_action) ) {
-    if (!useAction[dataObject.use_action.type]) {
-      useAction[dataObject.use_action.type] = [dataObject.use_action]
-    } else {
-      useAction[dataObject.use_action.type].push(dataObject.use_action)
-    }
-  } else if (dataObject?.use_action) {
-    console.log(dataObject.use_action);
-
-  }
+  // if (dataObject?.snippet_category && !Array.isArray(dataObject.snippet_category)) {
+  //   console.log(dataObject.snippet_category);
+  // }
+  // if (dataObject?.use_action && !Array.isArray(dataObject.use_action) ) {
+  //   if (!useAction[dataObject.use_action.type]) {
+  //     useAction[dataObject.use_action.type] = [dataObject.use_action]
+  //   } else {
+  //     useAction[dataObject.use_action.type].push(dataObject.use_action)
+  //   }
+  // } else if (dataObject?.use_action) {
+  //   console.log(dataObject.use_action);
+  // }
+  // if (dataObject?.material && typeof dataObject.material !== 'string') {
+  //   console.log(dataObject.material)
+  // }
 });
 
-
-fs.writeJsonSync(
-  out,
-  useAction,
-);
+fs.writeJsonSync(out, contents);
--- src/services/Descriptions/cdda/types.ts
+++ src/services/Descriptions/cdda/types.ts
@@ -1,74 +1,129 @@
 export enum CDDA_JSON_TYPES {
-  MOD_INFO = 'MOD_INFO',
-  MONSTER_BLACKLIST = 'MONSTER_BLACKLIST',
-  relic_procgen_data = 'relic_procgen_data',
-  ascii_art = 'ascii_art',
-  profession = 'profession',
-  scenario = 'scenario',
-  start_location = 'start_location',
-  furniture = 'furniture',
-  gate = 'gate',
-  terrain = 'terrain',
-  trap = 'trap',
-  item_group = 'item_group',
-  ammunition_type = 'ammunition_type',
-  AMMO = 'AMMO',
-  ARMOR = 'ARMOR',
-  BIONIC_ITEM = 'BIONIC_ITEM',
-  BOOK = 'BOOK',
-  GENERIC = 'GENERIC',
-  TOOL = 'TOOL',
-  COMESTIBLE = 'COMESTIBLE',
-  GUNMOD = 'GUNMOD',
-  GUN = 'GUN',
-  TOOL_ARMOR = 'TOOL_ARMOR',
-  harvest = 'harvest',
-  SPELL = 'SPELL',
-  MONSTER_FACTION = 'MONSTER_FACTION',
-  monstergroup = 'monstergroup',
-  MONSTER = 'MONSTER',
-  SPECIES = 'SPECIES',
-  speech = 'speech',
-  dream = 'dream',
-  mutation_category = 'mutation_category',
-  mutation = 'mutation',
-  talk_topic = 'talk_topic',
-  faction = 'faction',
-  mission_definition = 'mission_definition',
-  npc_class = 'npc_class',
-  npc = 'npc',
-  trait_group = 'trait_group',
-  mapgen = 'mapgen',
-  ter_furn_transform = 'ter_furn_transform',
-  overmap_special = 'overmap_special',
-  overmap_terrain = 'overmap_terrain',
-  region_overlay = 'region_overlay',
-  city_building = 'city_building',
-  requirement = 'requirement',
-  recipe = 'recipe',
-  recipe_category = 'recipe_category',
-  uncraft = 'uncraft',
-  enchantment = 'enchantment',
   achievement = 'achievement',
   ammo_effect = 'ammo_effect',
+  anatomy = 'anatomy',
   bionic = 'bionic',
+  body_part = 'body_part',
+  butchery_requirement = 'butchery_requirement',
   clothing_mod = 'clothing_mod',
+  conduct = 'conduct',
+  construction = 'construction',
+  construction_category = 'construction_category',
+  construction_group = 'construction_group',
+  MONSTER_BLACKLIST = 'MONSTER_BLACKLIST',
+  disease_type = 'disease_type',
+  dream = 'dream',
   effect_type = 'effect_type',
   emit = 'emit',
+  enchantment = 'enchantment',
   field_type = 'field_type',
   json_flag = 'json_flag',
+  gate = 'gate',
+  harvest = 'harvest',
+  hit_range = 'hit_range',
+  item_action = 'item_action',
+  ITEM_CATEGORY = 'ITEM_CATEGORY',
+  SPELL = 'SPELL',
+  LOOT_ZONE = 'LOOT_ZONE',
   martial_art = 'martial_art',
-  material = 'material',
-  scent_type = 'scent_type',
-  skill = 'skill',
-  snippet = 'snippet',
+  mutation = 'mutation',
   technique = 'technique',
+  material = 'material',
+  monster_attack = 'monster_attack',
+  GUN = 'GUN',
+  MONSTER_FACTION = 'MONSTER_FACTION',
+  morale_type = 'morale_type',
+  movement_mode = 'movement_mode',
+  MONSTER = 'MONSTER',
+  recipe = 'recipe',
+  TOOL = 'TOOL',
+  ARMOR = 'ARMOR',
+  GUNMOD = 'GUNMOD',
+  GENERIC = 'GENERIC',
+  BIONIC_ITEM = 'BIONIC_ITEM',
   vehicle_part = 'vehicle_part',
-  overlay_order = 'overlay_order',
-  mod_tileset = 'mod_tileset',
-  palette = 'palette',
+  ammunition_type = 'ammunition_type',
+  AMMO = 'AMMO',
+  fault = 'fault',
+  TOOLMOD = 'TOOLMOD',
+  terrain = 'terrain',
+  skill = 'skill',
+  obsolete_terrain = 'obsolete_terrain',
+  activity_type = 'activity_type',
+  item_group = 'item_group',
+  profession_item_substitutions = 'profession_item_substitutions',
+  profession = 'profession',
+  region_settings = 'region_settings',
+  vehicle_placement = 'vehicle_placement',
+  vehicle_spawn = 'vehicle_spawn',
+  rotatable_symbol = 'rotatable_symbol',
+  scenario = 'scenario',
+  scent_type = 'scent_type',
+  score = 'score',
+  skill_display_type = 'skill_display_type',
+  SPECIES = 'SPECIES',
+  speech = 'speech',
+  start_location = 'start_location',
   event_statistic = 'event_statistic',
   event_transformation = 'event_transformation',
+  overmap_terrain = 'overmap_terrain',
+  furniture = 'furniture',
+  tool_quality = 'tool_quality',
+  trap = 'trap',
+  vehicle_group = 'vehicle_group',
+  vitamin = 'vitamin',
+  weather_type = 'weather_type',
+  MOD_INFO = 'MOD_INFO',
+  EXTERNAL_OPTION = 'EXTERNAL_OPTION',
+  mod_tileset = 'mod_tileset',
+  monstergroup = 'monstergroup',
+  overmap_connection = 'overmap_connection',
+  overmap_location = 'overmap_location',
+  region_overlay = 'region_overlay',
+  MONSTER_WHITELIST = 'MONSTER_WHITELIST',
+  monster_adjustment = 'monster_adjustment',
+  city_building = 'city_building',
+  palette = 'palette',
+  mapgen = 'mapgen',
+  TOOL_ARMOR = 'TOOL_ARMOR',
+  MAGAZINE = 'MAGAZINE',
+  COMESTIBLE = 'COMESTIBLE',
+  BOOK = 'BOOK',
+  PET_ARMOR = 'PET_ARMOR',
+  ter_furn_transform = 'ter_furn_transform',
+  uncraft = 'uncraft',
+  vehicle = 'vehicle',
+  skill_boost = 'skill_boost',
+  overmap_special = 'overmap_special',
+  mutation_category = 'mutation_category',
+  snippet = 'snippet',
+  npc_class = 'npc_class',
+  trait_group = 'trait_group',
+  faction = 'faction',
+  npc = 'npc',
+  talk_topic = 'talk_topic',
+  requirement = 'requirement',
+  proficiency = 'proficiency',
+  recipe_category = 'recipe_category',
+  WHEEL = 'WHEEL',
+  mutation_type = 'mutation_type',
+  mission_definition = 'mission_definition',
+  colordef = 'colordef',
+  ITEM_BLACKLIST = 'ITEM_BLACKLIST',
+  MIGRATION = 'MIGRATION',
+  map_extra = 'map_extra',
+  recipe_group = 'recipe_group',
+  SCENARIO_BLACKLIST = 'SCENARIO_BLACKLIST',
+  TRAIT_BLACKLIST = 'TRAIT_BLACKLIST',
+  ENGINE = 'ENGINE',
+  vehicle_part_category = 'vehicle_part_category',
+  overmap_land_use_code = 'overmap_land_use_code',
+  charge_removal_blacklist = 'charge_removal_blacklist',
+  behavior = 'behavior',
+  overlay_order = 'overlay_order',
+  BATTERY = 'BATTERY',
+  ascii_art = 'ascii_art',
+  relic_procgen_data = 'relic_procgen_data',
 }
 
 interface PocketDaum {
@@ -85,6 +140,11 @@ interface PocketDaum {
   flag_restriction?: string[];
   min_item_volume?: string;
   ammo_restriction?: AmmoRestriction;
+}
+
+interface Brewable {
+  time: string;
+  results: string[];
 }
 
 export interface AmmoRestriction {
@@ -147,10 +207,19 @@ export interface AmmoRestriction {
 }
 
 type Name = string | { str: string; str_pl?: string };
+interface SpName {
+  str_sp: string;
+}
 type Color = string | string[];
 type RangeOrFixed = number | number[];
 interface IntermittentActivation {
   effects: Effect[];
+}
+
+interface ConditionalName {
+  type: string;
+  condition: string;
+  name: Name | SpName;
 }
 
 interface Effect {
@@ -179,6 +248,12 @@ interface Proportional {
   range?: number;
   encumbrance?: number;
   warmth?: number;
+  calories?: number;
+  quench?: number;
+  price_postapoc?: number;
+  parasites?: number;
+  healthy?: number;
+  fun?: number;
 }
 
 interface SnippetCategory {
@@ -207,6 +282,8 @@ interface Relative {
   recoil?: number;
   bashing?: number;
   material_thickness?: number;
+  vitamins?: ItemShortHand[];
+  fun?: number;
 }
 
 interface Explosion {
@@ -214,11 +291,22 @@ interface Explosion {
   shrapnel: number;
 }
 
+interface ArmorData {
+  covers: string[];
+  sided: boolean;
+  coverage: number;
+  encumbrance: number;
+  max_encumbrance: number;
+  material_thickness: number;
+}
+
 interface SeedData {
   fruit: string;
-  '//': string;
   plant_name: string;
   grow: string;
+  byproducts?: string[];
+  fruit_div?: number;
+  '//'?: string;
 }
 
 interface RelicData {
@@ -673,6 +761,20 @@ interface Countdown {
   message: string;
 }
 
+interface Milling {
+  into: string;
+  conversion_rate: number;
+}
+
+export interface Contamination {
+  disease: string;
+  probability: number;
+}
+
+export interface AmmoData {
+  ammo_type: string;
+}
+
 interface Inscribe {
   type: 'inscribe';
   verb: string;
@@ -989,7 +1091,7 @@ export type ICDDAJSON =
     }
   | {
       id?: string;
-      type: string;
+      type: CDDA_JSON_TYPES.ARMOR;
       name: Name;
       description?: string;
       weight?: string;
@@ -1033,4 +1135,114 @@ export type ICDDAJSON =
       power_armor?: boolean;
       longest_side?: string;
       properties?: string[][];
+    }
+  | {
+      id?: string;
+      type: CDDA_JSON_TYPES.BOOK;
+      name: Name;
+      description: string | { str: string; '//~': string };
+      'copy-from'?: string;
+      weight?: string;
+      volume?: string;
+      price?: number;
+      price_postapoc?: number;
+      material?: string[];
+      symbol?: string;
+      color?: string;
+      skill?: string;
+      required_level?: number;
+      max_level?: number;
+      intelligence?: number;
+      time?: string;
+      abstract?: string;
+      looks_like?: string;
+      chapters?: number;
+      fun?: number;
+      longest_side?: string;
+      bashing?: number;
+      flags?: string[];
+      relative?: Relative;
+      '//'?: string;
+      use_action?: string[];
+      ascii_picture?: string;
+      to_hit?: number;
+      martial_art?: string;
+      '//isbn13'?: number;
+      '//bashing'?: string;
+      snippet_category?: SnippetCategory[];
+      '//genre'?: string;
+      '//note'?: string;
+      '//dual genre'?: string;
+    }
+  | {
+      type: string;
+      id?: string;
+      name: Name;
+      category?: string;
+      weight?: string;
+      color?: string;
+      /**
+       * Like "4 days 16 hours" or just 1176
+       */
+      spoils_in: number | string;
+      comestible_type?: string;
+      symbol?: string;
+      quench?: number;
+      healthy?: number;
+      calories?: number;
+      description?: string;
+      /**
+       * Sometimes be "272 cent", but most time it is number
+       */
+      price: number | string;
+      /**
+       * Sometimes be "4 USD" or "95 cent", but most time it is number
+       */
+      price_postapoc: number | string;
+      material: string | string[];
+      volume?: string;
+      charges?: number;
+      stack_size?: number;
+      fun?: number;
+      'copy-from'?: string;
+      use_action: UseAction | UseAction[];
+      container?: string;
+      sealed?: boolean;
+      '//'?: string;
+      phase?: string;
+      freezing_point?: number;
+      conditional_names?: ConditionalName[];
+      flags?: string[];
+      '//2'?: string;
+      stim?: number;
+      looks_like?: string;
+      explode_in_fire?: boolean;
+      explosion?: Explosion;
+      abstract?: string;
+      addiction_type?: string;
+      addiction_potential?: number;
+      proportional?: Proportional;
+      armor_data?: ArmorData;
+      pocket_data?: PocketDaum[];
+      primary_material?: string;
+      vitamins?: ItemShortHand[];
+      brewable?: Brewable;
+      parasites?: number;
+      smoking_result?: string;
+      delete?: Delete;
+      cooks_like?: string;
+      snippet_category: SnippetCategory[] | string;
+      relative?: Relative;
+      extend?: Extend;
+      milling?: Milling;
+      fatigue_mod?: number;
+      contamination?: Contamination[];
+      ammo_data?: AmmoData;
+      rot_spawn?: string;
+      rot_spawn_chance?: number;
+      tool?: string;
+      '//TODO'?: string;
+      to_hit?: number;
+      bashing?: number;
+      seed_data?: SeedData;
     };

dbbfafefbbea9ab23b5784629c60139bc4e6c344
Lin Onetwo <linonetwo012@gmail.com>
2020-12-11 23:44:46 +0800

    refactor: make path fits headless mode

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,7 +1,13 @@
 import path from 'path';
+import os from 'os';
 
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const isProductionTest = process.env.PROD_TEST === 'true';
-export const resourcesPath = isDevelopment || isProductionTest ? path.join(__dirname, '../../', 'resources') : process.resourcesPath;
+let resourcesPath = path.join(os.tmpdir(), 'fallbackResourcePath');
+if (isDevelopment || isProductionTest) {
+  resourcesPath = path.join(__dirname, '../../', 'resources');
+} else if (typeof process !== 'undefined' && typeof process.resourcesPath === 'string') {
+  resourcesPath = process.resourcesPath;
+}
 export const iconPath = path.join(resourcesPath, 'icon.icns');
 export const modFolder = path.join(resourcesPath, 'mods');

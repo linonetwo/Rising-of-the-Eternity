1a618d882eab5d74ac0e9eb7c2fa75b41edb9e18
Lin Onetwo <linonetwo012@gmail.com>
2020-12-11 23:43:52 +0800

    chore: script to run services

---
+++ scripts/headlessStart/index.ts
@@ -0,0 +1,6 @@
+import { container } from '@/services';
+import { IElectronMainThreadService } from '@/services/Main';
+import SERVICE_IDENTIFIER from '@/services/identifiers';
+
+const main = container.get<IElectronMainThreadService>(SERVICE_IDENTIFIER.Main);
+void main.init();
